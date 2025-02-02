d14d641876cc46851f18120ce9b3482765c412b5
mahmoud-adel-ahmed <mahmoudalghareeb253@gmail.com>
2025-02-03 00:29:07 +0200

    first commit 9

--- middleware.ts
+++ middleware.ts
@@ -2,12 +2,9 @@ import { NextResponse, NextRequest } from "next/server";
 
 export function middleware(req: NextRequest) {
   const { pathname } = req.nextUrl;
-  const response = NextResponse.next();
-  response.headers.set("Cache-Control", "no-store, max-age=0");
 
   if (
-    pathname.startsWith("/auth/verify") ||
-    pathname.startsWith("/auth/resetpassword")
+    pathname.startsWith("/auth/verify")
   ) {
     const token = req.cookies.get("token");
     // console.log("🚀 ~ middleware ~ token:", token)
@@ -21,5 +18,5 @@ export function middleware(req: NextRequest) {
 
 // Apply the middleware to only the /verify route
 export const config = {
-  matcher: ["/auth/verify", "/auth/resetpassword"],
+  matcher: ["/auth/verify"],
 };
